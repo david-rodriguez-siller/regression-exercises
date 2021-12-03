4f70b3c562f69daa40b5c69c4617c7a892a05299
“David <David.rodriguez.siller@gmail.com>
2021-12-03 09:41:46 -0600

    Scaling exercise & wrangle file update

--- wrangle.py
+++ wrangle.py
@@ -24,3 +24,103 @@ def wrangle_zillow():
     df = df[df['calculatedfinishedsquarefeet'] > 200]
     
     return df
+
+def minmax_scaler_tvt(train, validate, test):
+    # list of columns float and int dtypes
+    num_cols = list(train.select_dtypes(include = ['float64', 'int64', 'complex']).columns)
+    
+    # min-max scaler object
+    scaler = sklearn.preprocessing.MinMaxScaler()
+    
+    # fit scaler
+    scaler.fit(train[num_cols])
+    
+    # scale
+    train_scaled = scaler.transform(train[num_cols])
+    validate_scaled = scaler.transform(validate[num_cols])
+    test_scaled = scaler.transform(test[num_cols])
+
+   # new column names
+    new_column_names = [c + '_scaled' for c in num_cols]
+
+    # add scaled columns to input dataset
+    train[new_column_names] = scaler.transform(train[num_cols])
+    validate[new_column_names] = scaler.transform(train[num_cols])
+    test[new_column_names] = scaler.transform(train[num_cols])
+    
+    return train, validate, test
+
+def standard_scaler_tvt(train, validate, test):
+    # list of columns float and int dtypes
+    num_cols = list(train.select_dtypes(include = ['float64', 'int64', 'complex']).columns)
+    
+    # standard scaler object
+    scaler = sklearn.preprocessing.StandardScaler()
+    
+    # fit scaler
+    scaler.fit(train[num_cols])
+    
+    # scale
+    train_scaled = scaler.transform(train[num_cols])
+    validate_scaled = scaler.transform(validate[num_cols])
+    test_scaled = scaler.transform(test[num_cols])
+
+    # new column names
+    new_column_names = [c + '_scaled' for c in num_cols]
+
+    # add scaled columns to input dataset
+    train[new_column_names] = scaler.transform(train[num_cols])
+    validate[new_column_names] = scaler.transform(train[num_cols])
+    test[new_column_names] = scaler.transform(train[num_cols])
+    
+    return train, validate, test
+
+def robust_scaler_tvt(train, validate, test):
+    # list of columns float and int dtypes
+    num_cols = list(train.select_dtypes(include = ['float64', 'int64', 'complex']).columns)
+    
+    # robust scaler object
+    scaler = sklearn.preprocessing.RobustScaler()
+    
+    # fit scaler
+    scaler.fit(train[num_cols])
+    
+    # scale
+    train_scaled = scaler.transform(train[num_cols])
+    validate_scaled = scaler.transform(validate[num_cols])
+    test_scaled = scaler.transform(test[num_cols])
+
+    # new column names
+    new_column_names = [c + '_scaled' for c in num_cols]
+
+    # add scaled columns to input dataset
+    train[new_column_names] = scaler.transform(train[num_cols])
+    validate[new_column_names] = scaler.transform(train[num_cols])
+    test[new_column_names] = scaler.transform(train[num_cols])
+    
+    return train, validate, test
+
+def nonlinear_scaler_tvt(train, validate, test):
+    # list of columns float and int dtypes
+    num_cols = list(train.select_dtypes(include = ['float64', 'int64', 'complex']).columns)
+    
+    # non-linear scaler object
+    scaler = sklearn.preprocessing.QuantileTransformer(output_distribution = 'normal')
+    
+    # fit scaler
+    scaler.fit(train[num_cols])
+    
+    # scale
+    train_scaled = scaler.transform(train[num_cols])
+    validate_scaled = scaler.transform(validate[num_cols])
+    test_scaled = scaler.transform(test[num_cols])
+    
+    # new column names
+    new_column_names = [c + '_scaled' for c in num_cols]
+
+    # add scaled columns to input dataset
+    train[new_column_names] = scaler.transform(train[num_cols])
+    validate[new_column_names] = scaler.transform(train[num_cols])
+    test[new_column_names] = scaler.transform(train[num_cols])
+
+    return train, validate, test
